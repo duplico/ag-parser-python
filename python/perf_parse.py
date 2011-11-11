e7b7ac2fa2541bc2858634dbc499b5bc38bf6a7f
George Louthan <duplico@dupli.co>
2011-11-10 22:17:05 -0600

    Add more performance results.

--- python/perf_parse.py
+++ python/perf_parse.py
@@ -1,8 +1,52 @@
-#>out = !ls
 import pstats
-out_string = ''
-for filename in out:
-    p = pstats.Stats(filename)
-    print filename, p.strip_dirs().stats[(
-        'ag_generator.py', 1158, 'main'
-    )][3] # cumulative time in main function
+import os
+import re
+
+fn_re = re.compile(r'perfd(?P<trial>\d+)(.+[/\\])+(?P<name>.*)_(?P<assets>\d+)_d(?P<depth>\d+)\.pstats')
+out_dict = dict()
+prepend_paths = map(lambda a: os.path.join('perfd%i' % a, 'd'), range(6))
+#prepend_paths = map(lambda a: 'perf%i' % a, range(6))
+for parent_path in prepend_paths:
+    print '---- TRIAL %s ----' % parent_path
+    for filename in os.listdir(parent_path):
+        filename = os.path.join(parent_path, filename)
+        p = pstats.Stats(filename)
+        m = fn_re.match(filename)
+        depth = m.group('depth')
+        num_assets = m.group('assets') # unused
+        scenario_name = m.group('name')
+        trial = m.group('trial')
+        time = p.strip_dirs().stats[(
+            'ag_generator.py', 1158, 'main'
+        )][3] # cumulative time in main function
+        
+        print 'Name %s-%s, assets %s, depth %s: time %0.2f' % (scenario_name, 
+                                                            trial, num_assets, 
+                                                            depth, time)
+        
+        depth = int(depth)
+        trial = int(trial)
+        out_dict.setdefault(depth, dict())
+        out_dict[depth].setdefault(scenario_name, dict())
+        out_dict[depth][scenario_name][trial] = time
+
+print out_dict
+
+lines = []
+heading = []
+for depth in sorted(out_dict.keys()):
+    if not heading:
+        new_heading = ['depth']
+    new_line = [str(depth)]
+    for scenario in out_dict[depth]:
+        for trial in sorted(out_dict[depth][scenario].keys()):
+            time = out_dict[depth][scenario][trial]
+            new_line.append(str(time))
+            if not heading:
+                new_heading.append('%s (%i)' % (scenario, trial))
+    if not heading:
+        heading = ','.join(new_heading)
+        lines.append(heading)
+    new_line_str = ','.join(new_line)
+    lines.append(new_line_str)
+print '\n'.join(lines)
--- python/profile_suite.py
+++ python/profile_suite.py
@@ -1,12 +1,50 @@
-from ag_generator import main
 import cProfile
+import os
+import sys
 
-fb_dict = dict(trials=range(1,6), depth=10) # (1,6)
-td_dict = dict(trials=range(1,5), depth=10000) # (1,5)
-for scenario, details in dict(fullbunny=fb_dict, thesis_dash7=td_dict).items():
-    xp_file = 'examples/%s.xp' % scenario
-    depth = details['depth']
-    for trial in details['trials']:
-        nm_file = 'examples/%s_%i.nm' % (scenario, trial)
-        cProfile.run('main(nm_file, xp_file, depth, state_graph=True, viz_graph=False, viz_states=False)',
-                     'perf/%s_%i.pstats' % (scenario, trial))
+from ag_generator import main
+
+def profile_assets():
+    global nm_file, xp_file, depth # make exec work
+    fb_dict = dict(trials=range(1,6), depth=10) # (1,6)
+    td_dict = dict(trials=range(1,5), depth=10000) # (1,5)
+    if not os.path.exists('perf/a'):
+        os.makedirs('perf/a')
+    for scenario, details in dict(fullbunny=fb_dict, thesis_dash7=td_dict).items():
+        xp_file = 'examples/%s.xp' % scenario
+        depth = details['depth']
+        for trial in details['trials']:
+            nm_file = 'examples/%s_%i.nm' % (scenario, trial)
+            cProfile.run('main(nm_file, xp_file, depth, state_graph=True, viz_graph=False, viz_states=False)',
+                         'perf/a/%s_%i_d%i.pstats' % (scenario, trial, depth))
+
+def profile_depths():
+    global nm_file, xp_file, depth # Make exec work
+    fb_dict = dict(trial=3, depths=range(2,23,4)) # (1,6)
+    td_dict = dict(trial=3, depths=range(2,23,4)) # (1,5)
+    if not os.path.exists('perf/d'):
+        os.makedirs('perf/d')
+    for scenario, details in dict(fullbunny=fb_dict, thesis_dash7=td_dict).items():
+        xp_file = 'examples/%s.xp' % scenario
+        trial = details['trial']
+        for depth in details['depths']:
+            nm_file = 'examples/%s_%i.nm' % (scenario, trial)
+            cProfile.run('main(nm_file, xp_file, depth, state_graph=True, viz_graph=False, viz_states=False)',
+                         'perf/d/%s_%i_d%i.pstats' % (scenario, trial, depth))
+
+def script_main(assets=True, depths=True):
+    if assets:
+        profile_assets()
+    if depths:
+        profile_depths()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1 or '-h' in sys.argv or '--help' in sys.argv:
+        print 'Usage: profile_suite.py [-a] [-d] [-h|--help]'
+        print '   -a: Profile assets'
+        print '   -d: Profile depths'
+        print '   -h: Print this message.'
+        sys.exit()
+    do_profile_assets = '-a' in sys.argv
+    do_profile_depths = '-d' in sys.argv
+    script_main(do_profile_assets, do_profile_depths)
